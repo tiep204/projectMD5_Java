eef1c6ca9c134a1e1b5c52768697a485fde78f07
tiep204 <dangtrantiep240604@gmail.com>
2023-09-15 08:34:42 +0700

    cau truc

---
+++ src/main/java/ra/ProjectMd5newApplication.java
@@ -0,0 +1,13 @@
+package ra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMd5newApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectMd5newApplication.class, args);
+	}
+
+}
